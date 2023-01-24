f01dfea8c20a628fd2f01693c5d20f2ee2308a95
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2023-01-24 15:26:00 -0300

    design ready

---
+++ src/pages/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ProductGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 116px;
+`;
+
+
+
